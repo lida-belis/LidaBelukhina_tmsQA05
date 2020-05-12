c996190cb3f92c67bdaeebba4328791ed10149f2
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-12 22:39:26 +0300

    Update Task_9.java

--- src/tms/qa05/lessons_homework/lesson_7/task_9/Task_9.java
+++ src/tms/qa05/lessons_homework/lesson_7/task_9/Task_9.java
@@ -3,8 +3,9 @@ package tms.qa05.lessons_homework.lesson_7.task_9;
 public class Task_9 {
     public static void main(String[] args) {
         String[] arrayString = {"Forest", "Shop", "Library", "Dream", "Bookshop"};
-        int symbol = 5;
+        int symbol = 5; // suymbol -> minLengthToPtrint
 
+        // эту логику в отдельный метод
         for (int i = 0; i < arrayString.length; ++i) {
             if (arrayString[i].length() > symbol) {
 
