56cb7e15f03d4a8c63c6b848894670badf05b897
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-12 22:30:08 +0300

    Update Task_2.java

--- src/tms/qa05/lessons_homework/lesson_7/task_2/Task_2.java
+++ src/tms/qa05/lessons_homework/lesson_7/task_2/Task_2.java
@@ -1,5 +1,6 @@
 package tms.qa05.lessons_homework.lesson_7.task_2;
-
+ 
+// +
 public class Task_2 {
     public static void main(String[] args) {
         StringBuilder word = new StringBuilder("alucard");
